9135b89d31ccdcbabf59a42e87b426ae2bf43643
hasannirob307875 <64268615+hasannirob307875@users.noreply.github.com>
2020-05-19 22:22:01 +0600

    add my proposal webpage
    its a proposal webpage

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var head=document.querySelector("#head");
+var pic=document.querySelector("#photo");
+
+function text1(){
+
+   
+    pic.src ="love.jpg";
+    head.innerHTML="I love you too"
+}
+var head2=document.querySelector("#head2");
+var pic2=document.querySelector("#photo2");
+
+function text2(){
+
+   pic2.src="sad.jpg";
+   head2.innerHTML="but it doesn’t matter i love you and i will. Stay with me forever, please"
+}
